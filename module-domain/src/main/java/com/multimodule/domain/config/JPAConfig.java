71f42f8c3331b38d4596bf412183c100cc1f09da
Gangchon You <gchyoo@coopnc.com>
2025-06-13 14:17:25 +0900

    Add JPA configuration bean

---
+++ module-domain/src/main/java/com/multimodule/domain/config/JPAConfig.java
@@ -0,0 +1,22 @@
+package com.multimodule.domain.config;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+/** JPA 설정 */
+@Configuration
+public class JPAConfig {
+
+  @PersistenceContext
+  private EntityManager entityManager;
+
+  @Bean
+  public JPAQueryFactory jpaQueryFactory() {
+    return new JPAQueryFactory(entityManager);
+  }
+}
