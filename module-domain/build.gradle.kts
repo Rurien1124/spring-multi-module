a556ee9c32cdf3b7f76011f9508c55728f86e7f7
Gangchon You <gchyoo@coopnc.com>
2025-06-13 15:09:26 +0900

    Fix QueryDSL can't create QClass from entity

--- module-domain/build.gradle.kts
+++ module-domain/build.gradle.kts
@@ -8,7 +8,10 @@ plugins {
 dependencies {
 	implementation(rootProject.libs.bundles.springboot.core)
 	implementation(rootProject.libs.bundles.springboot.domain)
-	implementation(rootProject.libs.bundles.querydsl) { artifact { classifier = "jakarta" } }
+	implementation(rootProject.libs.querydsl.jpa) { artifact { classifier = "jakarta" } }
+
+	annotationProcessor(rootProject.libs.bundles.jpa.annotationprocessor)
+	annotationProcessor(rootProject.libs.querydsl.apt) { artifact { classifier = "jakarta" } }
 }
 
 tasks.jar {
